ea49bc66232ebd3ef037142ab040691e2728fb3c
Mateo12BN <mateobjulian33@gmail.com>
2026-06-15 23:25:10 -0500

    Add files via upload

---
+++ EjemploFor.java
@@ -0,0 +1,10 @@
+public class EjemploFor {
+    public static void main(String[] args) {
+        
+        // Todo se define dentro del paréntesis: (Inicio; Condición; Avance)
+        for (int contadorNumero = 1; contadorNumero <= 5; contadorNumero++) {
+            System.out.println("Contando con for: " + contadorNumero);
+        }
+        
+    }
+}
